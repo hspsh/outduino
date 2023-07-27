0341b84764d37fd514ec59ca449c2090eccf4663
Jakub Sadowski <github@vcc.earth>
2023-07-27 19:38:49 +0200

    add firmware not stub

---
+++ outduino-fw/include/pinDefs.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#define ESP_TX 37
+#define ESP_RX 36
+
+#define OUT1 12
+#define OUT2 21
+#define OUT3 38
+#define OUT4 41
+
+#define IN1 11
+#define IN2 13
+#define IN3 48
+#define IN4 42
+
+#define PWR1 14
+#define PWR2 47
+#define PWR3 39
+#define PWR4 41
+
+#define CURR1 5
+#define CURR2 4
+#define CURR3 2
+#define CURR4 1
+
+#define BOOT_B1 0
+#define B2 9
+#define B3 10
+
+
+#define SDA 6
+#define SCL 7
+
+#define CC1101_SCLK 16
+#define CC1101_SI 15
+#define CC1101_SO 17
+#define CC1101_CS 18
+#define CC1101_GDO0 8
--- outduino-fw/src/main.cpp
+++ outduino-fw/src/main.cpp
@@ -1,17 +1,111 @@
 #include <Arduino.h>
-// #include "USB.h"
+#include <pinDefs.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+QueueHandle_t event_msg_queue = NULL;
+
+void serialTask( void * parameter );
+void printTimestampOnPress(int input_num, long unsigned int timestamp);
+
+typedef struct {
+  int input_num;
+  long unsigned int timestamp;
+} outduino_evt_t;
+
 
 void setup() {
 
-  // productName("test");
-
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
   Serial.begin(115200);
   Serial.println("aasi");
+
+  //inilialize pins as input 
+  pinMode(IN1, INPUT);
+  pinMode(IN2, INPUT);
+  pinMode(IN3, INPUT);
+  pinMode(IN4, INPUT);
+
+  pinMode(BOOT_B1, INPUT);
+  pinMode(B2, INPUT);
+  pinMode(B3, INPUT);
+
+  //inilialize pins as output
+  pinMode(OUT1, OUTPUT);
+  pinMode(OUT2, OUTPUT);
+  pinMode(OUT3, OUTPUT);
+  pinMode(OUT4, OUTPUT);
+
+  pinMode(PWR1, OUTPUT);
+  pinMode(PWR2, OUTPUT);
+  pinMode(PWR3, OUTPUT);
+  pinMode(PWR4, OUTPUT);
+
+  //initialize pins as analog input
+
+  pinMode(CURR1, INPUT);
+  pinMode(CURR2, INPUT);
+  pinMode(CURR3, INPUT);
+  pinMode(CURR4, INPUT);
+
+  TwoWire i2c = TwoWire(0);
+  Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &i2c);
+
+  i2c.begin(SDA, SCL, 100000);
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.print("hello world");
+  display.display();
+
+
+
+  event_msg_queue = xQueueCreate( 10, sizeof( outduino_evt_t ) );
+
+  //attach interrupts to input pins
+  attachInterrupt(digitalPinToInterrupt(IN1), []{
+    outduino_evt_t evt = {
+      .input_num = IN1,
+      .timestamp = millis()
+    };
+    xQueueSendFromISR(event_msg_queue, &evt, NULL);
+    }, RISING);
+
+  attachInterrupt(digitalPinToInterrupt(IN2), []{
+    outduino_evt_t evt = {
+      .input_num = IN2,
+      .timestamp = millis()
+    };
+    xQueueSendFromISR(event_msg_queue, &evt, NULL);
+    }, RISING);
+
+  attachInterrupt(digitalPinToInterrupt(IN3), []{
+  outduino_evt_t evt = {
+    .input_num = IN3,
+    .timestamp = millis()
+  };
+  xQueueSendFromISR(event_msg_queue, &evt, NULL);
+  }, RISING);
+
+  attachInterrupt(digitalPinToInterrupt(IN4), []{
+  outduino_evt_t evt = {
+    .input_num = IN4,
+    .timestamp = millis()
+  };
+  xQueueSendFromISR(event_msg_queue, &evt, NULL);
+  }, RISING);
+
+  digitalWrite(OUT1, HIGH);
+  digitalWrite(OUT2, HIGH);
+
+  xTaskCreate( serialTask, "serial task",
+  3000, NULL, 2, NULL );
 }
 
 void loop() {
@@ -20,7 +114,20 @@ void loop() {
     sleep(1);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void serialTask( void * parameter ) {
+  outduino_evt_t evt;
+  while(1){
+    if(xQueueReceive(event_msg_queue, &evt, 1000/portTICK_PERIOD_MS) == pdTRUE){
+      printTimestampOnPress(evt.input_num, evt.timestamp);
+    } else {
+      Serial.println("heartbeat");
+    }
+  }
+}
+
+void printTimestampOnPress(int input_num, long unsigned int timestamp){
+  Serial.print("Input ");
+  Serial.print(input_num);
+  Serial.print(" pressed at ");
+  Serial.println(timestamp);
 }
